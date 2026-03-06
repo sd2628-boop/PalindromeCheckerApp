b3ad0556d101f06ad531eb8ddd707cc62600d39c
sd2628-boop <sd2628@srmist.edu.in>
2026-03-06 09:10:55 +0530

    Added UC6 implementation

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,34 +1,50 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
+
 
 public class PalindromeCheckerApp{
     public static void main(String[] args){
         System.out.print("Welcome to Palindrome Checker App Management System\n");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word: ");
+        System.out.print("Enter a string to check palindrome: ");
         String input = scanner.nextLine();
+
+
+        input = input.toLowerCase();
+
+
+        Queue<Character> queue = new LinkedList<>();
+
 
         Stack<Character> stack = new Stack<>();
 
+
         for (char c : input.toCharArray()) {
-            stack.push(c);
+            if (Character.isLetterOrDigit(c)) {
+                queue.add(c);
+                stack.push(c);
+            }
         }
+
 
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
 
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
         }
 
+
         if (isPalindrome) {
-            System.out.println(input + " is a Palindrome.");
+            System.out.println("\"" + input + "\" is a Palindrome.");
         } else {
-            System.out.println(input + " is NOT a Palindrome.");
+            System.out.println("\"" + input + "\" is NOT a Palindrome.");
         }
         scanner.close();
     }
