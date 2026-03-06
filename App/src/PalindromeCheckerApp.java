1389b05f31463696728e48bf590db9f697076bbf
sd2628-boop <sd2628@srmist.edu.in>
2026-03-06 09:27:34 +0530

    Added UC11 implementation

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,31 +2,40 @@
 import java.util.*;
 
 
-public class PalindromeCheckerApp {
+class PalindromeChecker {
 
-    public static boolean isPalindrome(String input) {
+    public boolean checkPalindrome(String input) {
         String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        int start = 0;
+        int end = normalized.length() - 1;
 
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) !=
-                    normalized.charAt(normalized.length() - 1 - i)) {
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
                 return false;
             }
+            start++;
+            end--;
         }
         return true;
     }
+}
+
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string to check palindrome: ");
+        PalindromeChecker checker = new PalindromeChecker();
         String input = scanner.nextLine();
-        boolean result = isPalindrome(input);
+        boolean result = checker.checkPalindrome(input);
+
         if (result) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not Palindrome");
         }
+
         scanner.close();
     }
 }
